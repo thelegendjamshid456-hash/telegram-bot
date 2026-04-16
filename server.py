f31c4e401234a9e2cb4d87b489b0725b672375a5
thelegendjamshid456 <thelegendjamshid456@gmail.com>
2026-04-16 16:04:07 +0530

    Telegram bot ready

---
+++ server.py
@@ -0,0 +1,32 @@
+﻿from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+TOKEN = os.environ.get("TOKEN", " 8799766843:AAGjQKYAsSRUQExYWcE1FB4yLEtUROqYyGk")
+URL = f"https://api.telegram.org/bot{TOKEN}"
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        reply = f"You said: {text}"
+
+        requests.post(f"{URL}/sendMessage", json={
+            "chat_id": chat_id,
+            "text": reply
+        })
+
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
